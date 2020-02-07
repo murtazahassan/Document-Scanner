d6228fe0599cef4991094136b505e6429c9786ef
murtazahassan <mhpr@live.co.uk>
2020-02-07 16:59:03 +0300

    Update utlis.py

--- utlis.py
+++ utlis.py
@@ -44,53 +44,25 @@ def reorder(myPoints):
 
     return myPointsNew
 
-## FINDING THE BIGGEST COUNTOUR ASSUING THAT IS THE SUDUKO PUZZLE
+
 def biggestContour(contours):
     biggest = np.array([])
     max_area = 0
     for i in contours:
         area = cv2.contourArea(i)
-        if area > 100:
+        if area > 5000:
             peri = cv2.arcLength(i, True)
             approx = cv2.approxPolyDP(i, 0.02 * peri, True)
             if area > max_area and len(approx) == 4:
                 biggest = approx
                 max_area = area
     return biggest,max_area
+def drawRectangle(img,biggest,thickness):
+    cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 0), thickness)
 
-## DRAW GRID TO SEE THE WARP PRESPECTIVE EFFICENCY (OPTIONAL)
-def drawGrid(img):
-    secW = int(img.shape[1]/9)
-    secH = int(img.shape[0]/9)
-    for i in range (0,9):
-        pt1 = (0,secH*i)
-        pt2 = (img.shape[1],secH*i)
-        pt3 = (secW * i, 0)
-        pt4 = (secW*i,img.shape[0])
-        cv2.line(img, pt1, pt2, (255, 255, 0),2)
-        cv2.line(img, pt3, pt4, (255, 255, 0),2)
-    return img
-
-## TO SPLIT THE IMAGE INTO 81 DIFFRENT IMAGES
-def splitBoxes(img):
-    rows = np.vsplit(img,9)
-    boxes=[]
-    for r in rows:
-        cols= np.hsplit(r,9)
-        for box in cols:
-            boxes.append(box)
-    return boxes
-
-## TO DISPLAY THE SOLUTION ON THE IMAGE
-def displayNumbers(img,numbers,color = (0,255,0)):
-    secW = int(img.shape[1]/9)
-    secH = int(img.shape[0]/9)
-    for x in range (0,9):
-        for y in range (0,9):
-            if numbers[(y*9)+x] != 0 :
-                 cv2.putText(img, str(numbers[(y*9)+x]),
-                               (x*secW+int(secW/2)-10, int((y+0.8)*secH)), cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                            2, color, 2, cv2.LINE_AA)
     return img
 
 def nothing(x):
@@ -99,10 +71,8 @@ def nothing(x):
 def initializeTrackbars(intialTracbarVals=0):
     cv2.namedWindow("Trackbars")
     cv2.resizeWindow("Trackbars", 360, 240)
-    cv2.createTrackbar("Threshold1", "Trackbars", 100,255, nothing)
-    cv2.createTrackbar("Threshold2", "Trackbars", 100, 255, nothing)
-
-
+    cv2.createTrackbar("Threshold1", "Trackbars", 200,255, nothing)
+    cv2.createTrackbar("Threshold2", "Trackbars", 200, 255, nothing)
 
 
 def valTrackbars():
