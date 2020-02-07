10d4a83016d376e8146a19885c1bc045fad61a29
murtazahassan <mhpr@live.co.uk>
2020-02-07 19:26:27 +0300

    Update DocumentScannerMain.py

--- DocumentScannerMain.py
+++ DocumentScannerMain.py
@@ -5,7 +5,7 @@ import utlis
 
 ########################################################################
 webCamFeed = True
-pathImage = "4.jpg"
+pathImage = "1.jpg"
 cap = cv2.VideoCapture(1)
 cap.set(10,160)
 heightImg = 640
@@ -17,17 +17,17 @@ count=0
 
 while True:
 
-    success, img = cap.read()
-    #img = cv2.imread("4.jpg")
-    img = cv2.resize(img, (widthImg, heightImg)) # RESIZE IMAGE TO MAKE IT A SQUARE IMAGE
+    if webCamFeed:success, img = cap.read()
+    else:img = cv2.imread(pathImage)
+    img = cv2.resize(img, (widthImg, heightImg)) # RESIZE IMAGE
     imgBlank = np.zeros((heightImg,widthImg, 3), np.uint8) # CREATE A BLANK IMAGE FOR TESTING DEBUGING IF REQUIRED
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # CONVERT IMAGE TO GRAY SCALE
     imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # ADD GAUSSIAN BLUR
-    thres=utlis.valTrackbars()
-    imgThreshold = cv2.Canny(imgBlur,thres[0],thres[1])
+    thres=utlis.valTrackbars() # GET TRACK BAR VALUES FOR THRESHOLDS
+    imgThreshold = cv2.Canny(imgBlur,thres[0],thres[1]) # APPLY CANNY BLUR
     kernel = np.ones((5, 5))
-    imgDial = cv2.dilate(imgThreshold, kernel, iterations=2)
-    imgThreshold = cv2.erode(imgDial, kernel, iterations=1)
+    imgDial = cv2.dilate(imgThreshold, kernel, iterations=2) # APPLY DILATION
+    imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
 
     ## FIND ALL COUNTOURS
     imgContours = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
@@ -36,39 +36,49 @@ while True:
     cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10) # DRAW ALL DETECTED CONTOURS
 
 
-     # FIND THE BIGGEST COUNTOUR AND USE IT AS SUDUKO
+    # FIND THE BIGGEST COUNTOUR
     biggest, maxArea = utlis.biggestContour(contours) # FIND THE BIGGEST CONTOUR
     if biggest.size != 0:
         biggest=utlis.reorder(biggest)
         cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20) # DRAW THE BIGGEST CONTOUR
-        print(biggest.shape)
         imgBigContour = utlis.drawRectangle(imgBigContour,biggest,2)
         pts1 = np.float32(biggest) # PREPARE POINTS FOR WARP
         pts2 = np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]]) # PREPARE POINTS FOR WARP
-        matrix = cv2.getPerspectiveTransform(pts1, pts2) # GER
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
         imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
 
-        imgDetectedDigits = imgBlank.copy()
+        #REMOVE 20 PIXELS FORM EACH SIDE
+        imgWarpColored=imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
+        imgWarpColored = cv2.resize(imgWarpColored,(widthImg,heightImg))
+
+        # APPLY ADAPTIVE THRESHOLD
         imgWarpGray = cv2.cvtColor(imgWarpColored,cv2.COLOR_BGR2GRAY)
-        imgAdaptiveThre= cv2.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2) # APPLY ADATIVE THRESHOLD
+        imgAdaptiveThre= cv2.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2)
         imgAdaptiveThre = cv2.bitwise_not(imgAdaptiveThre)
         imgAdaptiveThre=cv2.medianBlur(imgAdaptiveThre,3)
-        imageArray = ([img,imgDial,imgThreshold,imgContours],
+
+        # Image Array for Display
+        imageArray = ([img,imgGray,imgThreshold,imgContours],
                       [imgBigContour,imgWarpColored, imgWarpGray,imgAdaptiveThre])
 
     else:
-        imageArray = ([img,imgDial,imgThreshold,imgContours],
+        imageArray = ([img,imgGray,imgThreshold,imgContours],
                       [imgBlank, imgBlank, imgBlank, imgBlank])
 
-    stackedImage = utlis.stackImages(imageArray, 0.75)
+    # LABELS FOR DISPLAY
+    lables = [["Original","Gray","Threshold","Contours"],
+              ["Biggest Contour","Warp Prespective","Warp Gray","Adaptive Threshold"]]
+
+    stackedImage = utlis.stackImages(imageArray,0.75,lables)
     cv2.imshow("Result",stackedImage)
 
+    # SAVE IMAGE WHEN 's' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('s'):
         cv2.imwrite("Scanned/myImage"+str(count)+".jpg",imgWarpColored)
-        count += 1
         cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
                       (1100, 350), (0, 255, 0), cv2.FILLED)
         cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
                     cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
         cv2.imshow('Result', stackedImage)
         cv2.waitKey(300)
+        count += 1
